940ce70c3a1bbf66ac7db0f731ee5965b7f28848
Antal <antal0126@live.cn>
2017-12-28 09:59:19 +0800

    zuul

---
+++ zuul/src/main/java/cc/comac/springcloud/zuul/ZuulApplication.java
@@ -0,0 +1,16 @@
+package cc.comac.springcloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy             // 开启Zuul的功能
+@EnableEurekaClient
+@SpringBootApplication
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+}
